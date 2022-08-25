8a90048aaf9b2b893361b92e3984eab89f5b6376
Jonathan Voss <k98kurz@gmail.com>
2022-08-25 17:54:22 -0400

    expose misc to test context

--- tests/context.py
+++ tests/context.py
@@ -2,7 +2,7 @@ import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from clocks import interfaces
+from clocks import interfaces, misc
 from clocks.scalar import ScalarClock
 from clocks.vector import VectorClock, MapClock
 from clocks.chain import DynamicChainClock, AntichainChainClock, VariableChainClock
